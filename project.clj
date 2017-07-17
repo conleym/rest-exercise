846bb4c7bb34d60e1367e8c378a6eeb2d2d92cb8
Mike Conley <conasdf@gmail.com>
2017-07-17 16:31:21 -0400

    Project file cleanup.

--- project.clj
+++ project.clj
@@ -9,12 +9,11 @@
                  [org.clojure/data.csv "0.1.4"]
                  [compojure "1.6.0"]
                  ;; ring.middleware.logger seems to be
-                 ;; abandonware. Let's try this. Exclude old
-                 ;; tools.logging.
+                 ;; abandonware. Let's try this.
                  [ring-logger "0.7.7" :exclusions [org.clojure/tools.logging]]
                  ;; HTTP status code constants
                  [metosin/ring-http-response "0.9.0"]
-                 ;; JSON serialization (uses old ring-core)
+                 ;; JSON serialization.
                  [ring/ring-json "0.4.0" :exclusions [ring.ring-core]]
                  ;; Phone number handling. Appears to support E.164.
                  [com.googlecode.libphonenumber/libphonenumber "8.6.0"]]
@@ -22,7 +21,6 @@
   :plugins [[lein-ring "0.12.0"]]
   :ring {:handler rest-exercise.app/app
          :nrepl {:start? yes :port 4567}}
-  :main rest-exercise.storage
   :target-path "target/%s"
   :profiles {:uberjar {:aot :all}
              :dev {:dependencies [[ring/ring-mock "0.3.1"]]}})
